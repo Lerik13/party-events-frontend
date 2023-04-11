69630edeac577a0a3fa790adddce81718d746b7a
Valeria <lerik13@gmail.com>
2023-04-11 15:03:00 -0600

    EventPage: using getStaticProps + getStaticPaths for getting dynamic data /api/events/[slug]

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,12 +1,52 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config'
 
-const EventPage = () => {
+const EventPage = ({event}) => {
 	return (
 		<Layout>
-			<h1>EventPage</h1>
+			<h1>{event.name}</h1>
 		</Layout>
 	)
 }
 
 export default EventPage
+
+
+export async function getStaticPaths() {
+	const res = await fetch(`${API_URL}/api/events`)
+	const events = await res.json()
+	
+	const paths = events.map(evt => ({
+		params: {slug: evt.slug}
+	}))
+
+	return {
+		paths,
+		fallback: true // false: if resource does not found => show 404 ; true: => make a new request
+	}
+}
+
+export async function getStaticProps({ params: {slug} }) {
+	const res = await fetch(`${API_URL}/api/events/${slug}`)
+	const events = await res.json()
+
+	return {
+		props: {
+			event: events[0]
+		},
+		revalidate: 1
+	}
+}
+/*
+export async function getServerSideProps({ query: {slug} }) {
+	const res = await fetch(`${API_URL}/api/events/${slug}`)
+	const events = await res.json()
+
+	return {
+		props: {
+			event: events[0]
+		}
+	}
+}
+*/
