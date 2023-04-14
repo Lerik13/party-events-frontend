a523e599cf59f63469ac40e2b5282ade042343f9
Valeria <lerik13@gmail.com>
2023-04-14 16:55:41 -0600

    Pagination in EventsPage

---
+++ components/Pagination.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import { PER_PAGE } from '@/config'
+
+const Pagination = ({page, total}) => {
+	const lastPage = Math.ceil(total / PER_PAGE)
+
+	return (
+		<>
+			{page > 1 && (
+				<Link href={`/events?page=${page-1}`} className='btn-secondary'>
+					{'<<'} Prev
+				</Link>
+			)}
+			{page < lastPage && (
+				<Link href={`/events?page=${page+1}`} className='btn-secondary'>
+					Next {'>>'}
+				</Link>
+			)}
+		</>
+	)
+}
+
+export default Pagination
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem';
-import { API_URL } from '@/config';
+import { API_URL, PER_PAGE } from '@/config';
+import Pagination from '@/components/Pagination';
 
-const EventsPage = ({events}) => {
+const EventsPage = ({ events, page, total }) => {
+
 	return (
 		<Layout>
 			<h1>Events</h1>
@@ -12,18 +14,27 @@ const EventsPage = ({events}) => {
 			{events.map(evt => (
 				<EventItem key={evt.id} event={evt} />
 			))}
+
+			<Pagination page={page} total={total} />
 		</Layout>
 	)
 }
 
 export default EventsPage;
 
-export async function getStaticProps() {
-	const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-	const events = await res.json()
+export async function getServerSideProps({query: {page = 1}}) {
+	// Calculate start page
+	const start = +page === 1 ? 0 : (+page-1)*PER_PAGE
 
+	// Fetch total
+	const totalRes = await fetch(`${API_URL}/events/count`)
+	const total = await totalRes.json()
+
+	// Fetch events
+	const eventRes = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`)
+	const events = await eventRes.json()
+	
 	return {
-		props: {events},
-		revalidate: 1,	// revalidate data every 1 sec, if data has changed => update data
+		props: {events, page: +page, total },
 	}
 }
